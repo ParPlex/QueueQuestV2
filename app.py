8eeffd7c9cdcf3216fa408226bd3e646eafc57cf
ParPlex <127345501+ParPlex@users.noreply.github.com>
2026-04-10 14:27:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import numpy as np
 import importlib
 import pytz 
-
+#force restart cloud
 # Force reload of route_solver on every interaction
 import route_solver
 importlib.reload(route_solver)
